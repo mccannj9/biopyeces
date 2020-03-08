dfab89ce451360a3103cc60526dc7f426cba1dd1
Jamie <jamie.mccann@gmx.at>
2020-03-08 22:13:33 +0100

    Added paired fastq parser

--- seqio/Fastq.py
+++ seqio/Fastq.py
@@ -49,3 +49,57 @@ class Fastq(object):
             self.file = open(self.filename)
 
         self.exhausted = False
+
+
+class PairedFastq(object):
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.file = open(filename)
+        self.data = []
+        self.consumed = 0
+        self.exhausted = False
+
+    def consume(self):
+        self.buffer = self.file.readline()
+        if self.buffer == '':
+            self.exhausted = True
+            raise StopIteration
+        self.data.append(self.buffer.strip())
+
+    def __next__(self):
+        self.data = []
+        if self.exhausted:
+            raise StopIteration
+
+        for x in range(8):
+            self.consume()
+
+        # removing the '+' line
+        if self.data:
+            self.data.pop(2)
+            self.data.pop(-2)
+            f = self.data[:3]
+            r = self.data[3:]
+
+        self.consumed += 1
+
+        return (
+            Sequence(*f, file_format='fastq'),
+            Sequence(*r, file_format='fastq')
+        )
+
+    def __iter__(self):
+        return self
+
+    def close(self):
+        self.file.close()
+
+    def reset(self):
+        if not(self.file.closed):
+            self.file.seek(0)
+            self.__next__()
+        else:
+            self.file = open(self.filename)
+
+        self.exhausted = False
