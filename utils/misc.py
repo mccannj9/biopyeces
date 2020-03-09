2b33e8669efb7d76927f710ea93e29b7bd13a338
Jamie <jamie.mccann@gmx.at>
2020-03-09 17:55:17 +0100

    Adding utils module, plus smart stdin/file reader for piping to scripts

---
+++ utils/misc.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import sys
+import contextlib
+
+@contextlib.contextmanager
+def _smart_open(filename, mode='Ur'):
+    if filename == '-':
+        if mode is None or mode == '' or 'r' in mode:
+            fh = sys.stdin
+        else:
+            fh = sys.stdout
+    else:
+        fh = open(filename, mode)
+    try:
+        yield fh
+    finally:
+        if filename is not '-':
+            fh.close()
